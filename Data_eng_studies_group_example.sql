e73cd942eaa9e7a5afce61c3368616d53baba8a4
Caue Paiva <128509727+caue-paiva@users.noreply.github.com>
2024-11-05 19:12:47 -0300

    Update Data_eng_studies_group_example.sql

--- Data_eng_studies_group_example.sql
+++ Data_eng_studies_group_example.sql
@@ -1,23 +1,21 @@
-
 CREATE TABLE fato_vendas (
- id_venda SERIAL  PRIMARY KEY,
- 
- produto_id INT,
- CONSTRAINT produto_fk FOREIGN KEY produto_id REFERENCES dimensao_produto(produto_id),
- 
- loja_id INT,
- CONSTRAINT loja_fk FOREIGN KEY loja_id REFERENCES dimensao_loja(loja_id)
- 
- data_id INT,
- CONSTRAINT data_fk  FOREIGN KEY data_id REFERENCES dimensao_data(data_id)
-
- pagamento_id INT,
- CONSTRAINT pagamento_fk FOREIGN KEY pagamento_id REFERENCES dimensao_pagamento(pagamento_id),
-
- quantidade INT NOT NULL,
- preco DECIMAL(10,2) NOT NULL
+    id_venda SERIAL PRIMARY KEY,
+    
+    produto_id INT,
+    CONSTRAINT produto_fk FOREIGN KEY (produto_id) REFERENCES dimensao_produto(produto_id),
+    
+    loja_id INT,
+    CONSTRAINT loja_fk FOREIGN KEY (loja_id) REFERENCES dimensao_loja(loja_id),
+    
+    data_id INT,
+    CONSTRAINT data_fk FOREIGN KEY (data_id) REFERENCES dimensao_data(data_id),
+    
+    pagamento_id INT,
+    CONSTRAINT pagamento_fk FOREIGN KEY (pagamento_id) REFERENCES dimensao_pagamento(pagamento_id),
+    
+    quantidade INT NOT NULL,
+    preco DECIMAL(10,2) NOT NULL
 );
-
 
 
 
@@ -26,7 +24,7 @@ CREATE TABLE dimensao_produto (
 	nome VARCHAR(100) NOT NULL,
 	descricao VARCHAR(200),
 	categoria VARCHAR(50),
-	marca VARCHAR(50) NOT NULL,
+	marca VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE dimensao_loja (
